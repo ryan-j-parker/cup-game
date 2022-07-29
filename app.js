bdb50288a26b6db6a321b2b0cee33a7b6c62bdf9
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-07-29 13:10:38 -0700

    refactor randomIndex function for modularity and correctImage function for conciseness, add styling

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-// import functions and grab DOM elements
-
-// let state
-
-// set event listeners 
-// get user input
-// use user input to update state 
-// update DOM to reflect the new state
-
-const cupContainerOne = document.getElementById('cup-container-1');
-const cupContainerTwo = document.getElementById('cup-container-2');
-const cupContainerThree = document.getElementById('cup-container-3');
-
+//grabbing the DOM elements:
 const cupBtnOne = document.getElementById('cup-btn-1');
 const cupBtnTwo = document.getElementById('cup-btn-2');
 const cupBtnThree = document.getElementById('cup-btn-3');
@@ -22,65 +10,86 @@ const cupImgThree = document.getElementById('cup-img-3');
 const hidingPlaces = ['cupOne', 'cupTwo', 'cupThree'];
 
 const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
 const totalEl = document.getElementById('total');
-
-let correctGuesses = 0;
-let totalGuesses = 0;
 
 const resetButton = document.getElementById('reset-button');
 
-function randomCup() {
-  const hidingSpot = Math.floor(Math.random() * hidingPlaces.length);
-  const answer = hidingPlaces[hidingSpot];
-  return answer;
-}
+// let state - default values
+let correctGuesses = 0;
+let incorrectGuesses = 0;
+let totalGuesses = 0;
 
+//event listeners
 cupBtnOne.addEventListener('click', () => {
-  handleGuess(randomCup(), 'cupOne');
+    handleGuess(randomCup(), 'cupOne');
 });
 
 cupBtnTwo.addEventListener('click', () => {
-  handleGuess(randomCup(), 'cupTwo');
+    handleGuess(randomCup(), 'cupTwo');
 });
 
 cupBtnThree.addEventListener('click', () => {
-  handleGuess(randomCup(), 'cupThree');
+    handleGuess(randomCup(), 'cupThree');
 });
 
+// event listener for reset button
+resetButton.addEventListener('click', () => {
+    winsEl.textContent = 0;
+    lossesEl.textContent = 0;
+    totalEl.textContent = 0;
+    correctGuesses = 0;
+    incorrectGuesses = 0;
+    totalGuesses = 0;
+    clearScreen();
+});
 
+// function to get a random item from a random array
+function getRandomItem(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+    return item;
+}
+
+// uses array item to assign a value to hidingSpot  
+function randomCup() {
+    const hidingSpot = getRandomItem(hidingPlaces);
+    return hidingSpot;
+}
+
+// update DOM
 function handleGuess(correctSpot, userGuess) {
 
-  clearScreen();
+    clearScreen();
 
-  if (correctSpot === userGuess) {
-    correctGuesses++;
-    totalGuesses++;
-    winsEl.textContent = correctGuesses;
-  } else {
-    totalGuesses++;
-  }
+    if (correctSpot === userGuess) {
+        correctGuesses++;
+        totalGuesses++;
+        winsEl.textContent = correctGuesses;
+    } else {
+        totalGuesses++;
+        incorrectGuesses++;
+        lossesEl.textContent = incorrectGuesses;
+    }
 
-  if (correctSpot === 'cupOne') {
-    cupImgOne.src = './assets/cup-with-ball.png';
-  } else if (correctSpot === 'cupTwo') {
-    cupImgTwo.src = './assets/cup-with-ball.png';
-  } else {
-    cupImgThree.src = './assets/cup-with-ball.png';
-  }
-
-  totalEl.textContent = totalGuesses;
+    correctImage(correctSpot);
+    totalEl.textContent = totalGuesses;
 }
 
-resetButton.addEventListener('click', () => {
-  winsEl.textContent = 0;
-  totalEl.textContent = 0;
-  clearScreen();
-});
+// function to assign correct spot image to the correct spot 
+function correctImage(correctSpot) {
+    if (correctSpot === 'cupOne') {
+        cupImgOne.src = './assets/teacup-kitten.png';
+    } else if (correctSpot === 'cupTwo') {
+        cupImgTwo.src = './assets/teacup-kitten.png';
+    } else {
+        cupImgThree.src = './assets/teacup-kitten.png';
+    }
+}
 
+// function to restore cup images to default state 
 function clearScreen() {
-  cupImgOne.src = './assets/red-cup.png';
-  cupImgTwo.src = './assets/red-cup.png';
-  cupImgThree.src = './assets/red-cup.png';
+    cupImgOne.src = './assets/fancy-teacup.png';
+    cupImgTwo.src = './assets/fancy-teacup.png';
+    cupImgThree.src = './assets/fancy-teacup.png';
 }
-
-
